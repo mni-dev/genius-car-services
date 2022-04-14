d26648d48b71c2af4ea04e828cc98d1c858c2927
mni-dev <nazrulislam01531@gmail.com>
2022-04-14 17:20:23 +0600

    set env variables for firebaseConfig

---
+++ src/Pages/Login/Register/Register.js
@@ -0,0 +1,49 @@
+ 
+import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Register = () => {
+    const navigate = useNavigate();
+
+    const navigateLogin = () =>{
+        navigate('/login')
+    }
+
+    const handleRegister = event =>{
+        event.preventDefault();
+    }
+    return (
+        <div className='container mx-auto w-50'>
+            <h1>Please Register</h1>
+            <Form onSubmit={handleRegister}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type="password" placeholder="Confirm Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+                <p>Already have an account?<Link to='/login' className='text-danger text-decoration-none pe-auto' onClick={navigateLogin}> Please Login</Link></p>
+            </Form>
+        </div>
+    );
+};
+
+export default Register;
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDsh8prnq9ydTu0EjltaXIvldTuy1NW8xA",
-  authDomain: "genius-car-services-979fb.firebaseapp.com",
-  projectId: "genius-car-services-979fb",
-  storageBucket: "genius-car-services-979fb.appspot.com",
-  messagingSenderId: "706826076769",
-  appId: "1:706826076769:web:881b0741bba3a49836e020"
+  apiKey:process.env.REACT_APP_apiKey, 
+  authDomain:process.env.REACT_APP_authDomain, 
+  projectId:process.env.REACT_APP_projectId, 
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
